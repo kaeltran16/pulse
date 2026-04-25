49bf36ee76bc4acbafba02e19a6dccbe240a2474
Kael <khang.tran@mozox.com>
2026-04-25 16:05:49 +0700

    test: allow node-env exports for db tests

--- jest.config.js
+++ jest.config.js
@@ -1,4 +1,9 @@
 module.exports = {
   preset: 'jest-expo',
-  testPathIgnorePatterns: ['/node_modules/', '/.expo/'],
+  testPathIgnorePatterns: ['/node_modules/', '/.expo/', '/backend/'],
+  // Per-file `@jest-environment node` docblocks let DB suites swap the
+  // jest-expo default env for plain node so better-sqlite3 can load.
+  testEnvironmentOptions: {
+    customExportConditions: ['node', 'node-addons'],
+  },
 };
